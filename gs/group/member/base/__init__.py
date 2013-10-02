e65df555d309473e99e3236616a4f3a710c77826
Michael JasonSmith <mpj17@onlinegroups.net>
2013-10-02 11:16:33 -0700

    Switching to absolute_import

--- gs/group/member/base/__init__.py
+++ gs/group/member/base/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
 #lint:disable
-from utils import user_member_of_group, user_member_of_site, \
+from .utils import user_member_of_group, user_member_of_site, \
     user_admin_of_group, user_group_admin_of_group, \
     user_site_admin_of_group, user_division_admin_of_group, \
     user_participation_coach_of_group, member_id
