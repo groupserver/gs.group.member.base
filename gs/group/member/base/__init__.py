9fe57efacb776191893d13631dde0408291485e8
Michael JasonSmith <mpj17@onlinegroups.net>
2016-02-19 09:59:27 -0800

    Using a tuple for the import of the utils into the module __init__

--- gs/group/member/base/__init__.py
+++ gs/group/member/base/__init__.py
@@ -2,8 +2,8 @@
 from __future__ import absolute_import
 #lint:disable
 from .members import (AllMembers, FullMembers, )
-from .utils import user_member_of_group, user_member_of_site, \
-    user_admin_of_group, user_group_admin_of_group, \
-    user_site_admin_of_group, user_division_admin_of_group, \
-    user_participation_coach_of_group, member_id, get_group_userids
+from .utils import (
+    user_member_of_group, user_member_of_site, user_admin_of_group, user_group_admin_of_group,
+    user_site_admin_of_group, user_division_admin_of_group,  user_participation_coach_of_group,
+    member_id, get_group_userids, )
 #lint:enable
