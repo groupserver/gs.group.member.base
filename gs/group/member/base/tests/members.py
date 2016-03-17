8a5e5109167e8306b69089687e28b2ff8d8e1ded
Michael JasonSmith <mpj17@onlinegroups.net>
2016-03-17 08:58:41 -0700

    Adding more tests, failing one as expected

--- gs/group/member/base/tests/members.py
+++ gs/group/member/base/tests/members.py
@@ -31,6 +31,7 @@ class TestNormalMembers(TestCase):
     @patch('gs.group.member.base.members.UnverifiedMembers.subsetIds', new_callable=PropertyMock)
     def test_ptn_coach_removed(self, m_UM_sI, m_PM_sI, m_M_sI, m_MM_sI, m_BM_sI, m_AM_sI,
                                m_ptnCoachId, m_memberIds):
+        '''Ensure we remove the participation coach from the list of normal members'''
         m_UM_sI.return_value = m_PM_sI.return_value = m_M_sI.return_value = m_MM_sI.return_value = \
             m_BM_sI.return_value = m_AM_sI.return_value = set()
         m_ptnCoachId.return_value = 'dirk'
@@ -39,3 +40,43 @@ class TestNormalMembers(TestCase):
         r = n.subsetIds
 
         self.assertEqual(set(['dinsdale', ]), r)
+
+    @patch.object(NormalMembers, 'memberIds', new_callable=PropertyMock)
+    @patch.object(NormalMembers, 'ptnCoachId', new_callable=PropertyMock)
+    @patch('gs.group.member.base.members.AdminMembers.subsetIds', new_callable=PropertyMock)
+    @patch('gs.group.member.base.members.BlockedMembers.subsetIds', new_callable=PropertyMock)
+    @patch('gs.group.member.base.members.ModeratedMembers.subsetIds', new_callable=PropertyMock)
+    @patch('gs.group.member.base.members.Moderators.subsetIds', new_callable=PropertyMock)
+    @patch('gs.group.member.base.members.PostingMembers.subsetIds', new_callable=PropertyMock)
+    @patch('gs.group.member.base.members.UnverifiedMembers.subsetIds', new_callable=PropertyMock)
+    def test_ptn_coach_absent(self, m_UM_sI, m_PM_sI, m_M_sI, m_MM_sI, m_BM_sI, m_AM_sI,
+                              m_ptnCoachId, m_memberIds):
+        '''Ensure we cope with no participation coach'''
+        m_UM_sI.return_value = m_PM_sI.return_value = m_M_sI.return_value = m_MM_sI.return_value = \
+            m_BM_sI.return_value = m_AM_sI.return_value = set()
+        m_ptnCoachId.return_value = ''
+        m_memberIds.return_value = set(['dirk', 'dinsdale', ])
+        n = NormalMembers(MagicMock())
+        r = n.subsetIds
+
+        self.assertEqual(set(['dirk', 'dinsdale', ]), r)
+
+    @patch.object(NormalMembers, 'memberIds', new_callable=PropertyMock)
+    @patch.object(NormalMembers, 'ptnCoachId', new_callable=PropertyMock)
+    @patch('gs.group.member.base.members.AdminMembers.subsetIds', new_callable=PropertyMock)
+    @patch('gs.group.member.base.members.BlockedMembers.subsetIds', new_callable=PropertyMock)
+    @patch('gs.group.member.base.members.ModeratedMembers.subsetIds', new_callable=PropertyMock)
+    @patch('gs.group.member.base.members.Moderators.subsetIds', new_callable=PropertyMock)
+    @patch('gs.group.member.base.members.PostingMembers.subsetIds', new_callable=PropertyMock)
+    @patch('gs.group.member.base.members.UnverifiedMembers.subsetIds', new_callable=PropertyMock)
+    def test_ptn_coach_wrong(self, m_UM_sI, m_PM_sI, m_M_sI, m_MM_sI, m_BM_sI, m_AM_sI,
+                             m_ptnCoachId, m_memberIds):
+        '''Ensure we cope with poorly-configured participation coach'''
+        m_UM_sI.return_value = m_PM_sI.return_value = m_M_sI.return_value = m_MM_sI.return_value = \
+            m_BM_sI.return_value = m_AM_sI.return_value = set()
+        m_ptnCoachId.return_value = 'piranha'
+        m_memberIds.return_value = set(['dirk', 'dinsdale', ])
+        n = NormalMembers(MagicMock())
+        r = n.subsetIds
+
+        self.assertEqual(set(['dirk', 'dinsdale', ]), r)
